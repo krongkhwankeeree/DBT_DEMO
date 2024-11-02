8454a00f886aae4702087cf56f2b83ebcc4eee9b
Krongkhwan Keereethammawong <keeree.kkn@gmail.com>
2024-11-02 23:42:14 +0000

    test

--- models/sales_table.sql
+++
@@ -1,2 +0,0 @@
-SELECT *, (`Quantity Ordered` * `Price Each`) AS total_price
-FROM `dbt-kkn.dbt_demo.salse`
--- models/total_revenue.sql
+++
@@ -1,3 +0,0 @@
-SELECT SUM(total_price) AS total_rev
-FROM {{ ref('sales_table') }}
-WHERE Refunded IS FALSE
